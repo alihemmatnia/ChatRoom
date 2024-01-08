022e14e4f58d5715297998bcb2bbcfd9edcc0179
alihemmatnia <ali.hemmatnia@gmail.com>
2024-01-08 10:49:56 +0330

    add dockerfile

---
+++ src/Server/ChatRoom.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("jfr");
+		}
+	}
+}
--- src/Server/ChatRoom.Api/Program.cs
+++ src/Server/ChatRoom.Api/Program.cs
@@ -10,10 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
 	app.UseSwagger();
 	app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
 }
 
 app.UseHttpsRedirection();
